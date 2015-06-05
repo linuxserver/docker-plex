14016abd0def7acd6c76d6a0194be38a71659e50
Stian Buch Larsen <lonixx@gmail.com>
2015-06-05 14:33:40 +0200

    Update 20_update_plex.sh
    err backwards me.

--- init/20_update_plex.sh
+++ init/20_update_plex.sh
@@ -2,9 +2,9 @@
 export DEBIAN_FRONTEND=noninteractive
 INSTALLED=`dpkg-query -W -f='${Version}' plexmediaserver`
 if [ "$PLEXPASS" -eq "1" ]; then
-	VERSION=$(curl https://raw.githubusercontent.com/linuxserver/misc-files/master/plex-version/public)
-else
 	VERSION=$(curl https://raw.githubusercontent.com/linuxserver/misc-files/master/plex-version/plexpass)
+else
+	VERSION=$(curl https://raw.githubusercontent.com/linuxserver/misc-files/master/plex-version/public)
 fi
 if [ "$VERSION" == "$INSTALLED" ]; then
 exit 0;
