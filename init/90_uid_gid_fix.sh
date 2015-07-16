a601ba4d0a72ebb7b65c390403399bf6797ba61e
Patrick Double <pat@patdouble.com>
2015-07-16 16:34:12 -0500

    Set config permissions

--- init/90_uid_gid_fix.sh
+++ init/90_uid_gid_fix.sh
@@ -2,6 +2,9 @@
 
 if [ "$(id -u abc)" != "$PUID" ]; then usermod -u "$PUID" abc ; fi
 if [ "$(id -g abc)" != "$PGID" ]; then groupmod -o -g  "$PGID" abc ; fi
+chown -R abc:abc /config
+chmod ug+rw /config
+find /config -type d -print0 | xargs -0 chmod ug+rwx
 
 echo "
 -----------------------------------
