bf142b17af66b85fd6b6e706eae74e6faecc6fd8
ljm42 <larry_meaney@iname.com>
2015-07-17 22:32:47 -0700

    Update 99_chown_plex_owned_files.sh
    update conditional

--- init/99_chown_plex_owned_files.sh
+++ init/99_chown_plex_owned_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ -f "/config/Library/Application Support" ]; then
+if [ -d "/config/Library/Application Support" ]; then
 find "/config/Library/Application Support" \! -user abc -exec chown -h abc:abc {} \;
 find "/config/Library/Application Support" \! -group abc -exec chown -h abc:abc {} \;
 fi
