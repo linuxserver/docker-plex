d6f5ee13e6f1f97f5f73b90b329cd3422f068f21
Stian Buch Larsen <lonixx@gmail.com>
2015-07-19 10:30:38 +0200

    Holy Crap, NO!
    This patch breaks The integrity of the plex database. Redacting

--- init/99_chown_plex_owned_files.sh
+++ init/99_chown_plex_owned_files.sh
@@ -4,5 +4,4 @@
 if [ -d "/config/Library" ]; then
 find "/config/Library" \! -user abc -exec chown -h abc:abc {} \;
 find "/config/Library" \! -group abc -exec chown -h abc:abc {} \;
-chmod -R ug+w "/config/Library"
 fi
