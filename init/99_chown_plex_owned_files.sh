9690b5c57b6380ca55b15abc67ae1e8027acf573
Stian Buch Larsen <lonixx@gmail.com>
2015-07-19 23:54:53 +0200

    Hopefully way faster chmodding

--- init/99_chown_plex_owned_files.sh
+++ init/99_chown_plex_owned_files.sh
@@ -2,8 +2,6 @@
 
 
 if [ -d "/config/Library" ]; then
-find "/config/Library" \! -user abc -exec chown -hv abc:abc {} \;
-find "/config/Library" \! -group abc -exec chown -hv abc:abc {} \;
-find "/config/Library/Application Support/Plex Media Server/Plug-ins" 
+find /config/Library ! \( -user abc -a -group abc \) -exec chown -hv abc:abc {}
 fi
 
