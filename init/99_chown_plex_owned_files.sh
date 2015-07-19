1a0559d72d7436c7260128a4d508f7c690b7f8f1
Stian Buch Larsen <lonixx@gmail.com>
2015-07-20 00:08:07 +0200

    Rename, and minor fix

--- init/99_chown_plex_owned_files.sh
+++ init/99_chown_plex_owned_files.sh
@@ -2,6 +2,6 @@
 
 
 if [ -d "/config/Library" ]; then
-find /config/Library ! \( -user abc -a -group abc \) -exec chown -hv abc:abc {}
+find /config/Library ! \( -user abc -a -group abc \) -exec chown -hv abc:abc {} \;
 fi
 
