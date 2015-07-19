cb56b0be155f2e2b2be29213c782b313a5d098dd
Stian Buch Larsen <lonixx@gmail.com>
2015-07-19 10:55:45 +0200

    Added -v to find commands, to show progress when you are converting.

--- init/99_chown_plex_owned_files.sh
+++ init/99_chown_plex_owned_files.sh
@@ -2,6 +2,8 @@
 
 
 if [ -d "/config/Library" ]; then
-find "/config/Library" \! -user abc -exec chown -h abc:abc {} \;
-find "/config/Library" \! -group abc -exec chown -h abc:abc {} \;
+find "/config/Library" \! -user abc -exec chown -hv abc:abc {} \;
+find "/config/Library" \! -group abc -exec chown -hv abc:abc {} \;
+find "/config/Library/Application Support/Plex Media Server/Plug-ins" 
 fi
+
