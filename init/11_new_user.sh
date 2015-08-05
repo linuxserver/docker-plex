f07f9d553fc9d58acb9cd8b59fe88523807c1ed8
Patrick Double <pat@patdouble.com>
2015-08-05 11:03:05 -0500

    Handle case of EUID or EGID not set

--- init/11_new_user.sh
+++ init/11_new_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$(id -u abc)" != "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
-if [ "$(id -g abc)" != "$PGID" ]; then groupmod -o -g  "$PGID" abc ; fi
+if [ -n "$PUID" -a "$(id -u abc)" != "$PUID" ]; then usermod -o -u "$PUID" abc ; fi
+if [ -n "$PGID" -a "$(id -g abc)" != "$PGID" ]; then groupmod -o -g  "$PGID" abc ; fi
 
 echo "
 -----------------------------------
