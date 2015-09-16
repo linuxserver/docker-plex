716149b8b6b3e93e187a6b1b19d594e2b255eb74
Stian Buch Larsen <lonixx@gmail.com>
2015-09-16 21:47:34 +0200

    New implentation of chowning

--- init/90_chown_plex_owned_files.sh
+++ init/90_chown_plex_owned_files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
-if [ -d "/config/Library" ]; then
-find /config/Library ! \( -user abc -a -group abc \) -exec chown -hv abc:abc {} +
-else
+if [ ! -d "/config/Library" ]; then
 mkdir /config/Library
 chown abc:abc /config/Library
 fi
 
+
+
+if [ ! -f "/config/Library/chown.log" ]; then
+	chown -Rc abc:root /config/Library >> /config/Library/chown.log
+fi
