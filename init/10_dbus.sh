fb251e9e98468b72820a9a21677e48add4afb071
Stian Buch Larsen <lonixx@gmail.com>
2015-11-24 10:01:57 +0100

    Reformated dbus

--- init/10_dbus.sh
+++ init/10_dbus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -e /var/run/dbus/pid ]; then rm -f /var/run/dbus/pid; fi
+[[ -e /var/run/dbus/pid ]] && rm -f /var/run/dbus/pid
+
 mkdir -p /var/run/dbus
 chown messagebus:messagebus /var/run/dbus
 dbus-uuidgen --ensure
