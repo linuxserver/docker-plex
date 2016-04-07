d7dc8223125739bc9dc283bced182bccee38f1f8
Stian Buch Larsen <lonixx@gmail.com>
2016-04-07 09:36:37 +0200

    Atempt of remove UUOC

--- init/30_update_plex.sh
+++ init/30_update_plex.sh
@@ -12,7 +12,7 @@ INSTALLED=$(dpkg-query -W -f='${Version}' plexmediaserver)
 
 
 #Get stuff from things.
-PLEX_TOKEN=$(cat "/config/Library/Application Support/Plex Media Server/Preferences.xml" | sed -n 's/.*PlexOnlineToken="//p' | sed "s/\".*//")
+PLEX_TOKEN=$( sed -n 's/.*PlexOnlineToken="//p' "/config/Library/Application Support/Plex Media Server/Preferences.xml" | sed "s/\".*//")
 [ -z $PLEX_TOKEN ] && echo "Plex token not avalible, please login " && exit 0
 PLEX_LATEST=$(curl -s "https://plex.tv/downloads/latest/1?channel=8&build=linux-ubuntu-x86_64&distro=ubuntu&X-Plex-Token=$PLEX_TOKEN"| cut -d "/" -f 5 )
 
